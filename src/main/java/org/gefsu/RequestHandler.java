dc4aab6662cc3448afcaaf4c0237cf215e2b50ae
Gefallener Soldat <hiimgefsu@gmail.com>
2022-10-19 20:42:01 +0300

    extractHttpVerb() method of RequestHandler now returns Optional<String>

--- src/main/java/org/gefsu/RequestHandler.java
+++ src/main/java/org/gefsu/RequestHandler.java
@@ -4,6 +4,7 @@ import org.gefsu.http.GetRequestReceiver;
 import org.gefsu.http.Receiver;
 import org.gefsu.http.BadRequestReceiver;
 import java.net.Socket;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,27 +13,22 @@ public class RequestHandler {
     public void processRequest(Socket clientSocket, String clientRequest) {
 
         Receiver receiver;
-        String httpVerb = extractHttpVerb(clientRequest);
+        Optional<String> httpVerb = extractHttpVerb(clientRequest);
 
         // If httpVerb is GET process it as a GET request.
-        if ((httpVerb != null) && httpVerb.equals("GET"))
-            receiver = new GetRequestReceiver(clientSocket, clientRequest);
-        // If anything else (including null) 400.
-        else
+        if (httpVerb.isEmpty() || !httpVerb.get().equals("GET"))
             receiver = new BadRequestReceiver(clientSocket, clientRequest);
+        else
+            receiver = new GetRequestReceiver(clientSocket, clientRequest);
 
         receiver.receive();
     }
 
-    private String extractHttpVerb(String clientRequest) {
+    private Optional<String> extractHttpVerb(String clientRequest) {
 
         Pattern pattern = Pattern.compile("^\\w+");
         Matcher matcher = pattern.matcher(clientRequest);
 
-        // Find an HTTP verb with regex. If no verb found 400.
-        if (matcher.find()) {
-            return(matcher.group().toUpperCase());
-        }
-        return null;
+        return Optional.of((matcher.group().toUpperCase()));
     }
 }
