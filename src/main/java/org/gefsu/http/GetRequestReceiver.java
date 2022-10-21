27db4b01e40342b31385db4c912753ab52e5a1d0
Gefallener Soldat <hiimgefsu@gmail.com>
2022-10-21 13:30:07 +0300

    Added various internal utility methods to GetRequestReceiver class. Removed old implementation of the receive() method.

--- src/main/java/org/gefsu/http/GetRequestReceiver.java
+++ src/main/java/org/gefsu/http/GetRequestReceiver.java
@@ -1,8 +1,8 @@
 package org.gefsu.http;
 
-import java.io.*;
+import java.io.File;
 import java.net.Socket;
-import java.util.Optional;
+import java.net.URISyntaxException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,62 +14,59 @@ public class GetRequestReceiver extends Receiver {
 
     @Override
     public void receive() {
-
-        // Try to get the InputStream of a requested resource.
-        // Write 200 OK and the content of that stream to the socket.
-        // If Stream is null write 404 to the socket.
-
-        getInputStreamOfResource(
-            extractResourceNameFromGetRequest(clientRequest))
-            .ifPresentOrElse(
-                x -> {
-                    try (var in = new BufferedReader(
-                        new InputStreamReader(x));
-                         var out = new BufferedWriter(
-                             new OutputStreamWriter(clientSocket.getOutputStream()))) {
-                        out.write("HTTP/1.1 200 OK\r\n");
-                        out.write("Content-Type: text/html\r\n");
-                        out.write("\r\n");
-                        in.transferTo(out);
-                    }
-                    catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }},
-
-                () -> {
-                    try (var out = new BufferedWriter(
-                        new OutputStreamWriter(clientSocket.getOutputStream()))) {
-                        out.write("HTTP/1.1 404 Not Found\r\n");
-                        out.write("Content-Type: text/html; charset=utf-8\r\n");
-                        out.write("\r\n");
-                        out.write(
-                            """
-                                <html><head>
-                                <title>404 Not Found</title>
-                                </head><body>
-                                <h1>Not Found</h1>
-                                <p>The requested URL was not found on this server.</p>
-                                </body></html>"""
-                        );
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+        // FIXME NOT IMPLEMENTED
     }
 
-    private String extractResourceNameFromGetRequest(String clientRequest) {
+    private String extractFileNameFromGetRequest(String clientRequest) {
 
         // Try to extract what resource is requested by GET
         Pattern pattern = Pattern.compile("(?<=^GET\\s)(\\S*)");
         Matcher matcher = pattern.matcher(clientRequest);
 
         if (matcher.find())
-            return (matcher.group().toLowerCase());
+            return matcher.group().toLowerCase();
 
         return "";
     }
 
-    private Optional<InputStream> getInputStreamOfResource(String fileName) {
-        return Optional.ofNullable((getClass().getResourceAsStream(fileName)));
+    private boolean fileExistsAndIsNotADirectory(String fileName)
+        throws URISyntaxException {
+
+        var url = getClass()
+            .getResource(fileName);
+
+        if (url != null) {
+            var file = new File(url.toURI());
+            return file.exists() && !file.isDirectory();
+        }
+        return false;
+    }
+
+    private MimeType getMimeTypeFromFileName(String fileName) {
+        return getMimeTypeFromExtension(extractExtensionFromFileName(fileName));
+    }
+
+    private String extractExtensionFromFileName(String fileName) {
+
+        Pattern pattern = Pattern.compile("\\.\\S+");
+        Matcher matcher = pattern.matcher(fileName);
+
+        if (matcher.find())
+            return matcher.group().toLowerCase();
+
+        return "";
+    }
+
+    private MimeType getMimeTypeFromExtension(String fileExtension) {
+        return (
+            switch (fileExtension) {
+                case "txt" -> MimeType.PLAINTEXT;
+                case "html" -> MimeType.HTML;
+                case "css" -> MimeType.CSS;
+                case "json" -> MimeType.JSON;
+                case "jpeg" -> MimeType.JPEG;
+                case "png" -> MimeType.PNG;
+                default -> MimeType.ARBITRARY;
+        });
     }
 }
