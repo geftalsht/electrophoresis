11471e215bba14e43d4a1b0901d5ea0c9351b267
gefsu <hiimgefsu@gmail.com>
2022-10-31 00:56:26 +0300

    Implemented map of responder objects

--- src/main/java/org/gefsu/RequestHandler.java
+++ src/main/java/org/gefsu/RequestHandler.java
@@ -3,13 +3,10 @@ package org.gefsu;
 import org.gefsu.http.exception.BadRequestException;
 import org.gefsu.http.request.HttpMethod;
 import org.gefsu.http.request.HttpParser;
-import org.gefsu.http.response.BadRequestResponder;
-import org.gefsu.http.response.Responder;
-
+import org.gefsu.http.response.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.HashMap;
 import java.util.Map;
 
 public class RequestHandler {
@@ -17,21 +14,30 @@ public class RequestHandler {
     private static final Map<HttpMethod, Responder> responderMap;
 
     static {
-        Map<HttpMethod, Responder> map = new HashMap<>();
-        map.put(HttpMethod.GET, );
+        var getResponder = new GetResponder();
+        var headResponder = new HeadResponder();
+        var notAllowedResponder = new NotAllowedResponder();
+
+        responderMap = Map.of(
+            HttpMethod.GET, getResponder,
+            HttpMethod.HEAD, headResponder,
+            HttpMethod.POST, notAllowedResponder,
+            HttpMethod.PUT, notAllowedResponder,
+            HttpMethod.DELETE, notAllowedResponder,
+            HttpMethod.CONNECT, notAllowedResponder,
+            HttpMethod.OPTIONS, notAllowedResponder,
+            HttpMethod.TRACE, notAllowedResponder
+        );
     }
 
     public static void handleClient(InputStream socketIn, OutputStream socketOut)
         throws IOException {
-
         try {
-
             var request = HttpParser.parseRequest(socketIn);
-            var responder = findResponder(request.getMethod().toString());
-
+            var responder = responderMap.get(request.getMethod());
+            responder.respond(request, socketOut);
         } catch (BadRequestException e) {
             BadRequestResponder.respond(socketOut);
         }
-
     }
 }
--- src/main/java/org/gefsu/http/response/HeadResponder.java
+++ src/main/java/org/gefsu/http/response/HeadResponder.java
@@ -3,7 +3,7 @@ package org.gefsu.http.response;
 import org.gefsu.http.request.HttpRequest;
 import java.io.OutputStream;
 
-public class GetResponder implements Responder {
+public class HeadResponder implements Responder {
 
     @Override
     public void respond(HttpRequest request, OutputStream socketOut) {
