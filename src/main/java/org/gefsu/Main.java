148a4db71f25d19cd550637a130d3bcf98466544
gefsu <hiimgefsu@gmail.com>
2022-11-03 13:42:09 +0300

    Added config file loading. Server now attempts to load the configuration file before starting. If the file is not found or the I/O error occurs while trying to load it, the server immediately terminates.

--- src/main/java/org/gefsu/Main.java
+++ src/main/java/org/gefsu/Main.java
@@ -1,9 +1,21 @@
 package org.gefsu;
 
 import java.util.Arrays;
+import java.util.Properties;
 
 public class Main {
+
+    private static final Properties CONFIG = new Properties();
     public static void main(String[] args) {
+
+        try (final var fin =
+                 Main.class.getResourceAsStream("/mimetypes.properies")) {
+            CONFIG.load(fin);
+        } catch (Exception e) {
+            System.out.println("Failed to read server configuration. Terminating.");
+            return;
+        }
+
         Arrays.stream(args)
             .findFirst()
             .flatMap(x -> OptionalUtils.lift(() -> Integer.parseInt(x)))
